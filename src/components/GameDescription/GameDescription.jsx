7447deeca5a1b9d9e45b8124b39922d65ee58260
NIKOLAOS SERGIS <78097143+SerNikos@users.noreply.github.com>
2025-12-09 11:49:18 +0200

    CREATED PORTAL FOR BUY MODAL

--- src/components/GameDescription/GameDescription.jsx
+++ src/components/GameDescription/GameDescription.jsx
@@ -1,42 +1,32 @@
 import "./GameDescription.css";
 import { useState } from "react";
+import { createPortal } from "react-dom";
 
 export default function GameDescription() {
   const [buy, setBuy] = useState(false);
-
-  function handleBuy() {
-    setBuy(true);
-  }
-
-  function handleClose() {
-    setBuy(false);
-  }
+  const modalRoot = document.getElementById("buy-modal");
 
   return (
     <div className="game-description">
       <h1 className="titleOfDiscription">YOU ARE UP NEXT</h1>
-      <p>
-        You have little but precious time to steal, trade, accuse, and do
-        anything else while you’re in the queue to prove that you’re the
-        protagonist and that you are NOT the next. "You Are Up Next" is a game
-        against the clock. Only one will be the winner, and the competition
-        comes not only from the other players but also from the executioner, who
-        threatens the condemned in line with death. Play smart, take risks, and
-        with a bit of luck you won’t be the next one to be executed.
-      </p>
-      <button className="buy-button" onClick={handleBuy}>
+      <p>You have little but precious time to steal, trade, accuse...</p>
+
+      <button className="buy-button" onClick={() => setBuy(true)}>
         Buy Now
       </button>
-      {buy && (
-        <div className="buy-box-container">
-          <div className="buy-box">
-            Soon on KICKSTARTER...
-            <button className="x-btn" onClick={handleClose}>
-              X
-            </button>
-          </div>
-        </div>
-      )}
+
+      {buy &&
+        createPortal(
+          <div className="modal-overlay" onClick={() => setBuy(false)}>
+            <div className="buy-box" onClick={(e) => e.stopPropagation()}>
+              Soon on KICKSTARTER...
+              <button className="x-btn" onClick={() => setBuy(false)}>
+                X
+              </button>
+            </div>
+          </div>,
+          modalRoot
+        )}
     </div>
   );
 }
